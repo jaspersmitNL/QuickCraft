c6a4226fd2518a73bca1a65a25fb438d51118219
= <jaspersmit2801@gmail.com>
2024-10-12 16:00:16 +0200

    Textures

--- source/game/world/render/WorldRenderer.hpp
+++ source/game/world/render/WorldRenderer.hpp
@@ -2,20 +2,31 @@
 #include "../World.hpp"
 #include <webgpu/webgpu_cpp.h>
 
-class WorldRenderer {
 
+struct TextureObject {
+    uint32_t idx;
+    std::string path;
+};
+
+
+class WorldRenderer {
 
 public:
     WorldRenderer(Ref<World> world): m_World(world) {}
 
+    void LoadTextures(std::vector<TextureObject> textures);
     void Init();
 
     void Render(wgpu::CommandEncoder& commandEncoder, wgpu::SurfaceTexture& surfaceTexture);
 
     wgpu::RenderPipeline m_Pipeline;
+    wgpu::Texture m_Texture;
+    wgpu::Sampler m_Sampler;
 private:
     Ref<World> m_World;
     wgpu::Buffer m_VertexBuffer;
     wgpu::ShaderModule m_Shader;
+
+
 
 };
